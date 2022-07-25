b421e9b85512eb9781db450e13e4d89e3997f487
AtesIsf <isfendiyar510@gmail.com>
2022-07-25 21:40:22 +0300

    Created set

---
+++ AtsLib/set.h
@@ -0,0 +1,25 @@
+#ifndef ATSSET_H
+#define ATSSET_H
+
+/*
+    Dynamic Array with no duplicates
+    Goal: Minimum space complexity (Slow)
+*/
+struct Set
+{
+    double *elements;
+    int size;
+};
+typedef struct Set set_t;
+
+/*
+    Initializes a set instance
+
+    Params: 
+    int size -> # of elements in the set
+
+    Returns:
+    set_t set -> The set instance
+*/
+
+#endif
